0a3ed1383d3be66a0823b08b2c73a7c0813f328b
Adam <as8513@nyu.edu>
2017-11-07 13:53:35 -0500

    Changes to the build script
    Get rid of verbosity from the go build, and change the server name to
    keep in line with what the compose file expects.

--- goServeMe/buildScript.sh
+++ goServeMe/buildScript.sh
@@ -1,7 +1,7 @@
-CGO_ENABLED=0 GOOS=linux go build -v -a -installsuffix cgo -o ./build/main .
+CGO_ENABLED=0 GOOS=linux go build -installsuffix cgo -o ./build/main .
 # CGO_ENABLED=0 disables dynamic lib links
 # GOOS=linux sets target OS to linux
 # -a means rebuild all the packages used in the program
 
-docker build -t webServer ./
+docker build -t website_server ./
 # Build a container for hosting our compiled code.
